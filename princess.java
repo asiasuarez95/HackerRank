1afc1ad97b3104ad4a61473bc828f3d7207438b2
HeySuarez <crashsoccer@gmail.com>
2016-05-23 21:51:32 -0400

    Add Comments

--- princess.java
+++ princess.java
@@ -1,3 +1,12 @@
+/**
+ * Complete the function displayPathtoPrincess which takes in two parameters 
+ * - the integer N and the character array grid. The grid will be formatted exactly as you see it in the input,
+ * so for the sample input the princess is at grid[2][0]. The function shall output moves (LEFT, RIGHT, UP or DOWN) 
+ * on consecutive lines to rescue/reach the princess. The goal is to reach the princess in as few moves as possible.
+ *
+ * @author Asia Suarez
+ * @since 5/23/2016
+ */
 import java.io.*;
 import java.util.*;
 import java.text.*;
@@ -5,6 +14,7 @@ import java.math.*;
 import java.util.regex.*;
 
 public class princess {
+
 static void displayPathtoPrincess(int n, String [] grid){
     int rp=0;
     int plocation=0;
